44b515ac83b2fd87ede656ead55c6b75dcb9e142
litianxing2016 <1530769634@qq.com>
2017-08-01 15:34:04 +0800

    test2

--- UIWangYi/AppDelegate.m
+++ UIWangYi/AppDelegate.m
@@ -25,6 +25,7 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     //test1
+    //test2
     //vc1
     ViewController1 *vc1 = [[ViewController1 alloc] init];
     UINavigationController *vc1Nav = [[UINavigationController alloc] initWithRootViewController:vc1];
